0d26c5d82ffbc457425885f37af4487ad88f1a3f
U-MA <g.0224incident@gmail.com>
2013-11-02 23:08:48 +0900

    p.42 硬貨の問題 テキストのソースも掲載

--- section2/coin.cpp
+++ section2/coin.cpp
@@ -4,18 +4,27 @@ using namespace std;
 int c1, c5, c10, c50, c100, c500; // コインの枚数
 int A; // 金額
 
+const int V[6] = { 1, 5, 10, 50, 100, 500 };
+int C[6];
+
 void input()
 {
     cin >> c1;
+    C[0] = c1;
     cin >> c5;
+    C[1] = c5;
     cin >> c10;
+    C[2] = c10;
     cin >> c50;
+    C[3] = c50;
     cin >> c100;
+    C[4] = c100;
     cin >> c500;
+    C[5] = c500;
     cin >> A;
 }
 
-void solve()
+void solve1()
 {
     int count = 0;
     while (A > 0)
@@ -56,9 +65,26 @@ void solve()
 }
 
 
+
+
+// テキストの手法
+void solve2()
+{
+    int ans = 0;
+
+    for (int i=5; i >= 0; i--)
+    {
+        int t = min(A / V[i], C[i]);
+        A -= t * V[i];
+        ans += t;
+    }
+    cout << ans << endl;
+}
+
+
 int main()
 {
     input();
-    solve();
+    solve2();
     return 0;
 }
